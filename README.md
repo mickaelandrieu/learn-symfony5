f081b45df82f061dabb27c1c151dd86dc943aade
Mickaël Andrieu <mickael.andrieu@solvolabs.com>
2021-11-22 08:08:04 +0100

    [OK] Live 4

--- README.md
+++ README.md
@@ -1,16 +1,50 @@
-# Live 3 : Création d'entités et utilisateur (Sécurité)
+# Live 4 : Espace public/privé moderne
 
-## Création d'entités
+## Création du formulaire de Login
 
-* `php bin/console make:entity`
+* `php bin/console make:controller Login`
 
-## Création du système utilisateur
+### Mise à jour du fichier `security.yaml`
 
-* `php bin/console make:user`
+```yaml
+security:
+    enable_authenticator_manager: true
+    password_hashers:
+        App\Entity\User:
+            algorithm: auto
 
-## Création du fichier de migration
+    providers:
+        app_user_provider:
+            entity:
+                class: App\Entity\User
+                property: email
+    firewalls:
+        dev:
+            pattern: ^/(_(profiler|wdt)|css|images|js)/
+            security: false
+        main:
+            lazy: true
+            provider: app_user_provider
+            form_login:
+                login_path: login
+                check_path: login
+                enable_csrf: true
 
-* `php bin/console make:migration`
+            logout:
+                path: app_logout
+                target: home
+
+    role_hierarchy:
+        ROLE_ADMIN: ROLE_USER
+
+    access_control:
+        - { path: ^/admin, roles: ROLE_ADMIN }
+
+```
+
+## Création du formulaire de création de comptes
+
+* `php bin/console make:registration-form`
 
 ## Mise à jour de la base de données
 
---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+            $userPasswordHasherInterface->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $user->setRoles(['ROLE_USER', 'ROLE_ADMIN']);
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+}
