904ddb74383c8627fabd5fd5134abde6878dbcf1
Mickaël Andrieu <mickael.andrieu@solvolabs.com>
2021-11-23 18:44:36 +0100

    Removed SecurityController

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -20,4 +20,10 @@ class LoginController extends AbstractController
             'error' => $error,
         ]);
     }
+
+    #[Route('/logout', name: 'logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
 }
